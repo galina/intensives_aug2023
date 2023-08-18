dfb1ae473771bd428e266a9e88e48916adb025ad
gl <galina.lezina@gmail.com>
2023-08-18 21:30:05 +0300

    Implement Task10Permutations

--- src/main/java/ru/yandex/tasks/Task10Permutations.java
+++ src/main/java/ru/yandex/tasks/Task10Permutations.java
@@ -1,14 +1,45 @@
 package ru.yandex.tasks;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Task10Permutations {
     public static int[][] permutations(int[] numbers) {
         /*
          * Возвращает массив со всеми перестановками массива numbers (массив n чисел от 1 до n, 1 <= n <= 10)
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        ArrayList<ArrayList<Integer>> permutations = new ArrayList<>();
+        for (int i = 0; i < numbers.length; ++i) {
+            ArrayList<Integer> current = new ArrayList<>();
+            current.add(numbers[i]);
+            permuteNums(numbers, permutations, current);
+        }
+
+        int[][] result = new int[permutations.size()][numbers.length];
+        for (int i = 0; i < permutations.size(); ++i) {
+            ArrayList<Integer> ith_permutation = permutations.get(i);
+            int[] datai = new int[numbers.length];
+            for (int j = 0; j < ith_permutation.size(); ++j) {
+                datai[j] = ith_permutation.get(j);
+            }
+            result[i] = datai;
+        }
+
+        return result;
+    }
+
+    public static void permuteNums(int[] nums, ArrayList<ArrayList<Integer>> permutations, ArrayList<Integer> current) {
+        if (current.size() == nums.length) {
+            permutations.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (!current.contains(nums[i])) {
+                current.add(nums[i]);
+                permuteNums(nums, permutations, current);
+                current.remove(current.size() - 1);
+            }
+        }
     }
 
     public static void selfCheck() {
