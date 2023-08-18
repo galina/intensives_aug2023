80c260a2f029886eaccfec9e0975ad77c34c972c
gl <galina.lezina@gmail.com>
2023-08-18 20:56:11 +0300

    Implement Task8BinarySearch

--- src/main/java/ru/yandex/tasks/Task8BinarySearch.java
+++ src/main/java/ru/yandex/tasks/Task8BinarySearch.java
@@ -10,8 +10,18 @@ public class Task8BinarySearch {
          * Выход: i, где numbers[i] = number, -1, если такого i нет
          * Если таких i несколько, вывести наибольший
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return -1;
+        int l = 0, r = sortedList.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (sortedList[mid] <= number) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        if (sortedList[l] != number)
+            return -1;
+        return l;
     }
 
     public static void selfCheck() {
