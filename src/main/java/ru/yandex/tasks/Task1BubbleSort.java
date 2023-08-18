2b3ceacbf9eb703e47793a18c7419203fc129024
gl <galina.lezina@gmail.com>
2023-08-18 18:21:55 +0300

    Implement Task1BubbleSort

--- src/main/java/ru/yandex/tasks/Task1BubbleSort.java
+++ src/main/java/ru/yandex/tasks/Task1BubbleSort.java
@@ -11,8 +11,24 @@ public class Task1BubbleSort {
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
          * Выход: отсортированный (сортировкой пузырьком!) numbers
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        int len = numbers.size();
+        while (len > 1) {
+            int swaps = 0;
+            for (int i = 0; i < len - 1; ++i) {
+                int j = i + 1;
+                if (numbers.get(i) > numbers.get(j)) {
+                    swaps += 1;
+                    int tmp = numbers.get(i);
+                    numbers.set(i, numbers.get(j));
+                    numbers.set(j, tmp);
+                }
+            }
+            if (swaps == 0) {
+                return numbers;
+            }
+            len -= 1;
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
