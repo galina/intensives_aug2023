d14ea1c37bf2fa6210893283ea1b2edccb4c97ea
gl <galina.lezina@gmail.com>
2023-08-18 19:49:47 +0300

    Implement Task6BraceBalance

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -2,13 +2,38 @@
 
 package ru.yandex.tasks;
 
+import java.util.Stack;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
         /*
          * sequence - последовательность скобок []{}() длины до 10^5
          * Выход: true/false, является ли строка ПСП
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+        String[] parts = s.split("");
+        Stack<String> stack = new Stack<>();
+
+        for (String part : parts) {
+            if (part.equals("(") || part.equals("{") || part.equals("[")) {
+                stack.push(part);
+                continue;
+            }
+            if (stack.isEmpty())
+                return false;
+            if (part.equals(")")) {
+                if (!stack.pop().equals("("))
+                    return false;
+            }
+            if (part.equals("}")) {
+                if (!stack.pop().equals("{"))
+                    return false;
+            }
+            if (part.equals("]")) {
+                if (!stack.pop().equals("["))
+                    return false;
+            }
+        }
+        if (stack.size() > 0) return false;
         return true;
     }
 
