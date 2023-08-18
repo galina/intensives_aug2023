7b9505a270587efdeba33e1932ee29e7b5142bab
gl <galina.lezina@gmail.com>
2023-08-18 20:29:51 +0300

    Implement Task7PrimeNumbers

--- src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
+++ src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
@@ -3,6 +3,7 @@
 package ru.yandex.tasks;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Task7PrimeNumbers {
     public static int[] findPrimes (int N) {
@@ -10,8 +11,26 @@ public class Task7PrimeNumbers {
          * 2 <= N <= 10^6
          * Выход: отсортированный массив всех простых чисел от 2 до N
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= N; ++i) {
+            if (isPrime(i)) {
+                primes.add(i);
+            }
+        }
+        int[] result = new int[primes.size()];
+        for (int i = 0; i < result.length; ++i) {
+            result[i] = primes.get(i);
+        }
+        return result;
+    }
+
+    public static boolean isPrime(int i) {
+        for (int j = 2; j < Math.sqrt(i); ++j) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void selfCheck() {
